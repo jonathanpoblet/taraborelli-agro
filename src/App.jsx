8fc9d541099da8d8996309b7168f112aed6fb924
unknown <jonathanpoblet228@gmail.com>
2023-07-10 09:11:06 -0300

    taraborelli agro initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Soon from './pages/Soon/Soon';
+
+function App() {
+  return (
+    <>
+      <Soon/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/Soon/Soon.jsx
@@ -0,0 +1,10 @@
+import './soon.css';
+
+export default function Soon() {
+  return (
+    <div className='soon'>
+        <img className='soon-img' src='../../../assets/taraborelli-agro-logo.png' alt='logo'/>
+        <h1 className='soon-title'>PRÓXIMAMENTE...</h1>
+    </div>
+  )
+}
