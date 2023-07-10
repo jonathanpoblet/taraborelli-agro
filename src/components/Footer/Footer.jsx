8548f4c55b51e51ed2bcf37ca630d4d85beded3d
unknown <jonathanpoblet228@gmail.com>
2023-07-10 15:31:38 -0300

    footer full responsive finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsFillTelephoneFill, BsFillEnvelopeFill } from 'react-icons/bs';
+import { MdLocationOn } from 'react-icons/md';
 import './footer.css';
 
 export default function Footer() {
+  const [footerOpen, setFooterOpen] = useState(false)
   return (
-    <footer>
-      <div>
-        <h3>Chivilcoy</h3>
-        <div>
-          <BsFillTelephoneFill/>
-          <h4>Tel: 02342 – 480359</h4>
+    <>
+      <footer id='footer' className='footer'>
+        <div className='footer-container'>
+          <img id='footer-container-img' className='footer-container-img' src='../../../public/assets/k.png' alt='K'/>
         </div>
-        <div>
-          <BsFillEnvelopeFill/>
-          <h4>De 8hs. a 19hs.</h4>
+        
+        <div className='footer-container'>
+          <h3 className='footer-container-title'>Chivilcoy</h3>
+          <div className='footer-container-info'>
+            <BsFillTelephoneFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Tel: 02342 – 480359</h4>
+          </div>
+          <div className='footer-container-info'>
+            <BsFillEnvelopeFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>De 8hs. a 19hs.</h4>
+          </div>
+          <div className='footer-container-info'>
+            <MdLocationOn className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Ruta Nac. Nº 5 – Km.154.5</h4>
+          </div>
         </div>
+
+        <div className='footer-container'>
+          <h3 className='footer-container-title'>BRAGADO</h3>
+          <div className='footer-container-info'>
+            <BsFillTelephoneFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Tel: 02342 – 430803</h4>
+          </div>
+          <div className='footer-container-info'>
+            <BsFillEnvelopeFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>De 8hs. a 19hs.</h4>
+          </div>
+          <div className='footer-container-info'>
+            <MdLocationOn className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Ruta Nac. Nº 5 – Km. 209.5</h4>
+          </div>
+        </div>
+
+        <div className='footer-container'>
+          <h3 className='footer-container-title'>ALBERTI</h3>
+          <div className='footer-container-info'>
+            <BsFillTelephoneFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Tel: 02346 – 470995</h4>
+          </div>
+          <div className='footer-container-info'>
+            <BsFillEnvelopeFill className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>De 8hs. a 12hs. y de 15hs. a 19hs.</h4>
+          </div>
+          <div className='footer-container-info'>
+            <MdLocationOn className='footer-container-info-icon' />
+            <h4 className='footer-container-info-text'>Ruta Nac. Nº 5 – Km. 190</h4>
+          </div>
+        </div>
+      </footer>
+
+      {
+        footerOpen &&
+          <div className='footer-mobile'>
+            <ul className='footer-mobile-ul'>
+              <li className='footer-mobile-ul-li'>
+                <a className='footer-mobile-ul-li-a' href='tel:2342480359'>Chivilcoy: 2342 - 480359</a>
+              </li>
+              <li className='footer-mobile-ul-li'>
+                <a className='footer-mobile-ul-li-a' href='tel:2346470995'>Alberti: 2346 - 470995</a>
+              </li>
+              <li className='footer-mobile-ul-li'>
+                <a className='footer-mobile-ul-li-a' href='tel:2342430803'>Bragado: 2342 - 430803</a>
+              </li>
+            </ul>
+          </div>
+      }
+
+      <div className='footer-mob'>
+        <BsFillTelephoneFill
+          className='footer-mob-button'
+          onClick={() => setFooterOpen(!footerOpen)}
+        />
       </div>
-      <div>
-        <h3>BRAGADO</h3>
-        <div>
-          <BsFillTelephoneFill/>
-          <h4>Tel: 02342 – 480359</h4>
-        </div>
-        <div>
-          <BsFillEnvelopeFill/>
-          <h4>De 8hs. a 19hs.</h4>
-        </div>
+
+      <div className='autor'>
+        <p className='autor-texto'>- KELLY - TODOS LOS DERECHOS RESERVADOS ®</p>
       </div>
-      <div>
-        <h3>ALBERTI</h3>
-        <div>
-          <BsFillTelephoneFill/>
-          <h4>Tel: 02342 – 480359</h4>
-        </div>
-        <div>
-          <BsFillEnvelopeFill/>
-          <h4>De 8hs. a 19hs.</h4>
-        </div>
-      </div>
-    </footer>
+    </>
   )
 }
