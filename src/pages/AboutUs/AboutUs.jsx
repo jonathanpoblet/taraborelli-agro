0586ab2f148cc1d0b1db9a5faa27eb212582a431
unknown <jonathanpoblet228@gmail.com>
2023-07-11 14:45:02 -0300

    about me added

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -5,13 +5,17 @@ export default function AboutUs() {
 
   return (
     <main className="main">
+      <section className='main-us'>
+          <article className='main-us-article'>
+            <img className='main-us-article-img' src='../.././../public/assets/truck.jpeg'/>
+            <div className='main-us-article-container'>
+              <p  id="info-text1">Somos una empresa familiar con más de 20 años de experiencia en el rubro de la <b>maquinaria agrícola</b>, comprometida con la entrega de soluciones integrales.<br/><br/>Nuestro líder Gabriel Kelly cuenta con más de 40 años de compromiso y experiencia en el sector agropecuario. Su socio estratégico Gabriel Romano, sumó a la fuerza de venta, el conocimiento y dedicación intensa de la posventa, y juntos conformaron la empresa Gabriel E. Kelly y Cia. S.A.</p>
+              <p  id="info-text2">Durante más de diez años la compañía se convirtió en concesionario oficial de maquinaria agrícola de una reconocida multinacional. Dentro de sus marcas comercializadas se destaca la empresa Bertini en sembradoras.<br/><br/>Actualmente, y luego de un proceso de reinvención, Kelly se convierte en concesionario oficial New Holland. Un nuevo desafío que nos permite continuar fortaleciendo nuestra pasión por la venta y <b>la cercanía con nuestro cliente</b>.</p>
+            </div>
+          </article>
+      </section>
       <section className="main-info">
         <article className="main-info-article">
-          <img src='../.././../public/assets/kelly-sucursal.jpeg'/>
-          <div>
-            <p id="info-text1">Somos una empresa familiar con más de 20 años de experiencia en el rubro de la maquinaria agrícola, comprometida con la entrega de soluciones integrales.<br/><br/>Nuestro líder Gabriel Kelly cuenta con más de 40 años de compromiso y experiencia en el sector agropecuario. Su socio estratégico Gabriel Romano, sumó a la fuerza de venta, el conocimiento y dedicación intensa de la posventa, y juntos conformaron la empresa Gabriel E. Kelly y Cia. S.A.</p>
-            <p id="info-text2">Durante más de diez años la compañía se convirtió en concesionario oficial de maquinaria agrícola de una reconocida multinacional. Dentro de sus marcas comercializadas se destaca la empresa Bertini en sembradoras.<br/><br/>Actualmente, y luego de un proceso de reinvención, Kelly se convierte en concesionario oficial New Holland. Un nuevo desafío que nos permite continuar fortaleciendo nuestra pasión por la venta y la cercanía con nuestro cliente.</p>
-          </div>
 
           <h2 id="title-info" >ELEGÍ NEW HOLLAND</h2>
           <h4 id="subtitle-info" >Te brindamos las mejores condiciones para que puedas adquirir tu producto</h4>
