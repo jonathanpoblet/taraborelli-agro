7ccfcaa1bf301cd1dcebbba85d3e05a87ce69f0f
unknown <jonathanpoblet228@gmail.com>
2023-07-14 14:24:47 -0300

    contact page added

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -6,40 +6,26 @@ export default function ContactSection() {
     <section className='contactSection'>
       <article className='contactSection-article'>
         <h2 className='contactSection-article-title'>CONTACTANOS</h2>
-        <form className='contactSection-article-form'>
-          <input className='contactSection-article-form-input' type='text' placeholder='Nombre'/>
-          <input className='contactSection-article-form-input' type='text' placeholder='Apellido'/>
-          <input className='contactSection-article-form-input' type='text' placeholder='Teléfono'/>
-          <input className='contactSection-article-form-input' type='text' placeholder='Correo'/>
-          <select className='contactSection-article-form-select'>
-            <option value='Asunto: Repuestos'>
-              Asunto: Repuestos
-            </option>
-            <option value='Asunto: Servicios'>
-              Asunto: Servicios
-            </option>
-            <option value='Asunto: Ventas'>
-              Asunto: Ventas
-            </option>
-            <option value='Asunto: RRHH'>
-              Asunto: RRHH
-            </option>
-          </select>
-          <textarea className='contactSection-article-form-textarea' placeholder='Mensaje'>
-
-          </textarea>
-          <button className='contactSection-article-form-button' type='button'>ENVIAR</button>
-        </form>
-      </article>
-
-
-
-      <article className='contactSection-article2'>
-        <h3>¿Querés trabajar con nosotros?</h3>
-        <p>Envianos tu Curriculum Vitae a rrhh@gkelly.com.ar o adjuntalo a continuación</p>
+        <h3>No dudes en escribirnos</h3>
+        <p>Atención personalizada en todo momento para que puedas accedar a tu maquinaria agricola ya mismo</p>
         <input type='file' />
         <button type='button'>ENVIAR</button>
-        <p>Seguinos en nuestras redes sociales</p>
+      </article>
+      
+      <article className='contactSection-article2'>
+        <h3 className='contactSection-article2-title'>Envianos un mensaje</h3>
+        <form className='contactSection-article2-form'>
+          <input className='contactSection-article2-form-input' type='text' placeholder='Nombre'/>
+          <input className='contactSection-article2-form-input' type='text' placeholder='Apellido'/>
+          <input className='contactSection-article2-form-input' type='text' placeholder='Teléfono'/>
+          <input className='contactSection-article2-form-input' type='text' placeholder='Correo'/>
+          <input className='contactSection-article2-form-input' type='text' placeholder='¿Tienes Auto Usado?'/>
+          <input className='contactSection-article2-form-input' type='text' placeholder='¿Qué modelo estas buscando?'/>
+          <textarea className='contactSection-article2-form-textarea' placeholder='Mensaje'>
+
+          </textarea>
+          <button className='contactSection-article2-form-button' type='button'>ENVIAR</button>
+        </form>
       </article>
     </section>
   )
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './contact.css';
+import ContactSection from '../../components/ContactSection/ContactSection';
 
 export default function Contact() {
   return (
@@ -7,6 +8,8 @@ export default function Contact() {
         <section className='contact-section1'>
             <img className='contact-section1-img' src='../../../public/assets/contact.png' alt='contact'/>
         </section>
+        
+        <ContactSection/>
     </main>
   )
 }
